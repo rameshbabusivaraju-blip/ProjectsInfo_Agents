8d2f19b3940a8f1fee6394845fb76f0a9659b453
SivarajuRB-web <326262155+SivarajuRB-web@users.noreply.github.com>
2026-09-23 10:06:21 +0530

    AGENTS-34 Updated tests

--- tests/test_db.py
+++ tests/test_db.py
@@ -21,5 +21,5 @@ def test_schema_creates_expected_tables() -> None:
         "commits",
         "pull_requests",
         "pr_reviews",
-         "confluence_pages",
+        "confluence_pages",
     }
